b21e967dc35bf0d1362fc64c257deeaa30ee1b6b
Reem Mehyar <2914429r@student.gla.ac.uk>
2025-02-14 00:40:29 +0000

    before login() view creation

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -20,8 +22,18 @@ class PageForm(forms.ModelForm):
                         help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
-    class Meta:
+class Meta:
 
-        model = Page
-        
-        exclude = ('category',)
+    model = Page
+    exclude = ('category',)
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
